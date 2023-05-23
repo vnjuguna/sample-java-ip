fa41d62070bc6d66c660af82c61fad4c10c7221f
Violet <vnjuguna@gmail.com>
2023-05-23 19:15:39 +0300

    made edits to cipherencoding class

--- src/main/java/org/indigo/Cipherencoding.java
+++ src/main/java/org/indigo/Cipherencoding.java
@@ -1,4 +1,21 @@
 package org.indigo;
 
 public class Cipherencoding {
+    private static String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+    public static String encode(String msg, int key){
+        String output = "";
+        for (char c: msg.toCharArray()){
+            int charPos = alphabets.indexOf(c);
+            if (charPos == -1){
+                output = output + c;
+                continue;
+            }
+            int tempNewPos = charPos + key;
+            int newPos = tempNewPos > 25? tempNewPos % 26 : tempNewPos;
+            char replacement = alphabets.charAt(newPos);
+            output = output + replacement;
+        }
+        return output;
+    }
+
 }
