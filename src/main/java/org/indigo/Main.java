2cc86b76d986024428131f557409a87325b109fc
Violet <vnjuguna@gmail.com>
2023-05-23 19:12:43 +0300

    made additional changes to Main

--- src/main/java/org/indigo/Main.java
+++ src/main/java/org/indigo/Main.java
@@ -7,25 +7,32 @@ public class Main {
         Scanner userInputObject = new Scanner(System.in);
         do {
             System.out.println("Select Process - e-encode/d-decode!");
-            String process = userInputObject.next();
-
+            String process = userInputObject.nextLine();
             System.out.println("Enter Message!");
-            String msg = userInputObject.next();
+            String msg = userInputObject.nextLine();
 
             System.out.println("Enter Key!");
             int key = userInputObject.nextInt();
 
             System.out.println("Processing...");
-            //TO DO add logic for encoding
+            System.out.println(process);
+            if (process.startsWith("e")){
+                String output = Cipherencoding.encode(msg, key);
+                System.out.println(output);
+            } else if (process.startsWith("d")){
+                System.out.println("decoding");
+            } else {
+                System.out.println("invalid process");
+                continue;
+            }
             //TO DO add logic for decoding
 
-            System.out.println("Do You Want to quit or would you like to continue!");
-            String nextStep = userInputObject.next();
+            System.out.println("Type q to quit or c to continue!");
+            String nextStep = userInputObject.nextLine();
 
             if (nextStep.startsWith("q")){
                 break;
             }
 
         } while (true);
-    }
-}
+    }}
