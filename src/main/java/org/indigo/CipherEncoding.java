bda3a68d96e2142299fea3c9660001a5c10bfe06
Violet <vnjuguna@gmail.com>
2023-05-31 13:07:04 +0300

    implemented encoding of lowercase characters

--- src/main/java/org/indigo/CipherEncoding.java
+++ src/main/java/org/indigo/CipherEncoding.java
@@ -1,18 +1,23 @@
 package org.indigo;
 
 public class CipherEncoding {
-    private static String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
     public static String encode(String msg, int key){
         String output = "";
         for (char c: msg.toCharArray()){
-            int charPos = alphabets.indexOf(c);
+            String alphabet=uppercaseAlphabets;
+            if (Character.isLowerCase(c)){
+             alphabet=lowercaseAlphabets;
+            }
+            int charPos = alphabet.indexOf(c);
             if (charPos == -1){
                 output = output + c;
                 continue;
             }
             int tempNewPos = charPos + key;
             int newPos = tempNewPos > 25? tempNewPos % 26 : tempNewPos;
-            char replacement = alphabets.charAt(newPos);
+            char replacement = alphabet.charAt(newPos);
             output = output + replacement;
         }
         return output;
