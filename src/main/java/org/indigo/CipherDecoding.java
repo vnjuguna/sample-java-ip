f2853264dddf292a770a437f958f3bd8ba3d3a3e
Violet <vnjuguna@gmail.com>
2023-06-29 19:24:00 +0300

    debugged the test class, corrected the errors

--- src/main/java/org/indigo/CipherDecoding.java
+++ src/main/java/org/indigo/CipherDecoding.java
@@ -1,9 +1,25 @@
 package org.indigo;
 
 public class CipherDecoding {
+    private static String uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
     public static String decode(String msg, int key){
         String output = "";
-
+        for (char c: msg.toCharArray()){
+            String alphabet=uppercaseAlphabets;
+            if (Character.isLowerCase(c)){
+                alphabet=lowercaseAlphabets;
+            }
+            int charPos = alphabet.indexOf(c);
+            if (charPos == -1){
+                output = output + c;
+                continue;
+            }
+            int tempNewPos = charPos - key;
+            int newPos = tempNewPos < 0? 26 + tempNewPos : tempNewPos;
+            char replacement = alphabet.charAt(newPos);
+            output = output + replacement;
+        }
         return output;
     }
 
--- src/test/java/org/indigo/CipherDecodingTest.java
+++ src/test/java/org/indigo/CipherDecodingTest.java
@@ -17,4 +17,17 @@ public class CipherDecodingTest {
         assertEquals("X", CipherDecoding.decode("Y", 1));
         assertEquals("Y", CipherDecoding.decode("Z", 1));
     }
+    @Test
+    @DisplayName("Caesar Cipher Decoding a Single LowerCase Character and No Wrap Around")
+    void decodeSingleLowerCaseCharacterNoWrapAround() {
+        assertEquals("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD",
+                CipherDecoding.decode("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG",3));
+        assertEquals("a", CipherDecoding.decode("b", 1));
+        assertEquals("c", CipherDecoding.decode("e", 2));
+        assertEquals("b", CipherDecoding.decode("i", 7));
+        assertEquals("t", CipherDecoding.decode("z", 6));
+        assertEquals("e", CipherDecoding.decode("h", 3));
+        assertEquals("x", CipherDecoding.decode("y", 1));
+        assertEquals("y", CipherDecoding.decode("z", 1));
+    }
 }
